c08445ef7eb0fb0f6ca495bc0880499e3412e510
z9877969 <z9877969@ukr.net>
2023-01-17 23:34:20 +0200

    adds base files to l-02

--- src/index.js
+++ src/index.js
@@ -3,26 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App";
 
-const container = React.createElement(
-  "div",
-  null,
-  React.createElement("h1", null, "Index"),
-  React.createElement(
-    "ul",
-    { style: { listStyle: "none" } },
-    React.createElement("li", null, "1"),
-    React.createElement("li", null, "2"),
-    React.createElement("li", null, "3")
-  )
-);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
